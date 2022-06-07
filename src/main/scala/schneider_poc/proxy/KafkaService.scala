26cdeab660b69270c5ee24c992bf46e42c79f971
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-06-07 20:52:42 +0300

    should be working

--- src/main/scala/schneider_poc/proxy/KafkaService.scala
+++ src/main/scala/schneider_poc/proxy/KafkaService.scala
@@ -13,26 +13,27 @@ trait KafkaService {
 }
 
 object KafkaService extends LazyLogging {
-  def live(bootstrapServer: String): KafkaService = new KafkaService {
-    val producer = new KafkaProducer[String, String](new Properties() {
-      put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer)
-      put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-      put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-    })
+  def live(bootstrapServer: String): KafkaService = (topic: String, key: String, message: String) => {
+    Task {
+      val producer = new KafkaProducer[String, String](new Properties() {
+        put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer)
+        put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+        put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+      })
 
-    override def produce(topic: String, key: String, message: String): Task[Unit] =
-      Task {
-        producer
-          .send(
-            new ProducerRecord(topic, key, message),
-            (metadata: RecordMetadata, exception: Exception) => {
-              if (exception == null)
-                logger.debug(s"Kafka message with key=$key and topic=$topic produced, offset=${metadata.offset()}")
-              else
-                throw exception
-            }
-          )
-          .get()
-      }
+      producer
+        .send(
+          new ProducerRecord(topic, key, message),
+          (metadata: RecordMetadata, exception: Exception) => {
+            producer.close()
+
+            if (exception == null)
+              logger.debug(s"Kafka message with key=$key and topic=$topic produced, offset=${metadata.offset()}")
+            else
+              throw exception
+          }
+        )
+        .get()
+    }
   }
 }
