900e9b2c8d37ca5f34545c51420eeb6bc220c181
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-06-08 00:52:42 +0300

    trying to populate clickhouse

--- init.sql
+++ init.sql
@@ -1,16 +1,15 @@
-create table if not exists mytopic_table(
+create table if not exists table_mytopic(
     `id`        String,
     `timestamp` Int64,
     `value`     Float64
 ) engine = MergeTree() order by `timestamp`;
 
-create table if not exists mytopic_queue(
+create table if not exists kafka_mytopic(
     `id`        String,
     `timestamp` Int64,
     `value`     Float64
-) engine = Kafka()
-settings
-    kafka_broker_list = 'kafka:9092',
+) engine = Kafka() settings
+    kafka_broker_list = '127.0.0.1:9092',
     kafka_topic_list = 'mytopic',
     kafka_group_name = 'mytopic-clickhouse-consumer-1',
     kafka_num_consumers = 5,
@@ -20,4 +19,4 @@ settings
     kafka_max_block_size = 1,
     kafka_format = 'JSONEachRow';
 
-create materialized view if not exists mytopic_mv to mytopic_table as select * from mytopic_queue;
+create materialized view if not exists view_mytopic to table_mytopic as select * from kafka_mytopic;
